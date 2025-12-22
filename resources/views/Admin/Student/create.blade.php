db5b1e74750cdd6b42655dd421a1192d73de49f0
tasneemaldernawi-oss <tasneemaldernawi@gmail.com>
2025-12-22 11:15:45 +0200

    creating student

---
+++ resources/views/Admin/Student/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<h3 class="mb-4">Add Student</h3>
+
+
+
+    <form action="{{ route('student.store') }}" method="POST">
+        @csrf
+
+        <div class="mb-3">
+            <label for="stNo" class="form-label">Student ID</label>
+            <input type="text" name="stNo" id="stNo" class="form-control" value="{{ old('stNo') }}" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" name="email" id="email" class="form-control" value="{{ old('email') }}"  required>
+        </div>
+
+        <div class="mb-3">
+            <label for="avg" class="form-label">GPA</label>
+            <input type="number" name="avg" id="avg" class="form-control" value="{{ old('avg') }}"  required>
+        </div>
+
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" id="password" class="form-control" value="{{ old('password') }}" required>
+        </div>
+    
+          <div class="mb-3">
+            <label for="status" class="form-label">Status</label>
+            <select name="status" required>
+                <option value="active">Active</option>
+                <option value="notActive">Not Active</option>
+                <option value="dismissed">Dismissed</option>
+
+            </select>
+        </div>
+
+
+
+        <button type="submit" class="btn btn-dark">Save</button>
+        <a href="{{ route('student.index') }}" class="btn btn-secondary">Cancel</a>
+    </form>
+
+@endsection
